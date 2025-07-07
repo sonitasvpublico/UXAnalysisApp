72f5c605dec84403077f6c38fdb8d2c767399ea1
Sonia Zavaleta Toukkari <soniazavaletatoukkari@Sonias-MacBook-Air.local>
2025-07-07 13:28:02 +0300

    Actualizar nombre visible y <title> a Nitida AI en todos los archivos relevantes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.sonitasv.uxanalysisapp',
-  appName: 'Nítida',
+  appName: 'Nitida AI',
   webDir: 'dist'
 };
 
